ea3bd8fa3024fb1bf1bef3bf5ad04b1e77aab991
Akash Bilgi <44390281+akashbilgi@users.noreply.github.com>
2022-11-26 18:48:22 -0800

    234. Palindrome Linked List - easy - added

---
+++ 234_Palindrome_Linked_List_Easy.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        l1 = []
+        curr = head
+        count = 0
+        while curr:
+            #if count <=
+            l1.append(curr.val)
+            count += 1
+            print(curr.val)
+            curr = curr.next
+        for i in range(count):
+            if l1[i] != l1[count-i-1]:return False
+        return True
